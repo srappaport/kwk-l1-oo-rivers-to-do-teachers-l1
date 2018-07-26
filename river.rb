d47e74cf92bcfbc878d78fca8d76c12c036a3f7b
srappaport <sarah.rappaport@greendot.org>
2018-07-26 13:46:27 +0000

    Done.

--- river.rb
+++ river.rb
@@ -1,1 +1,23 @@
 # river.rb
+class River
+  attr_accessor :length, :countries, :discharge
+  def initialize(name)
+    @name=name
+  end
+
+  def name
+    @name
+  end
+
+  def name= (new_name)
+    @name=new_name
+  end
+
+  def flood
+    @discharge = @discharge*1.3
+  end
+
+  def dry_up
+    @discharge = @discharge*0.5
+  end
+end
